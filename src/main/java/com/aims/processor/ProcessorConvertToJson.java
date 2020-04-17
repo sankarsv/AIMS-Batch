a9beb2406038f3fae8395e37b5181c7bfc099dea
user <user@192.168.1.9>
2020-04-17 16:29:57 +0530

    JSON object conversion

--- src/main/java/com/aims/processor/ProcessorConvertToJson.java
+++ src/main/java/com/aims/processor/ProcessorConvertToJson.java
@@ -7,6 +7,7 @@ import org.springframework.batch.item.ItemProcessor;
 
 import com.aims.bo.Employee;
 import com.aims.model.HCIntermediate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author ShanmugapriyaJ
@@ -17,6 +18,9 @@ public class ProcessorConvertToJson  implements ItemProcessor<Employee, HCInterm
 	@Override
 	public HCIntermediate process(Employee emp) throws Exception {
 		// TODO Auto-generated method stub
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = mapper.writeValueAsString(emp);
+	    System.out.println(jsonString);
 		return null;
 	}
 
